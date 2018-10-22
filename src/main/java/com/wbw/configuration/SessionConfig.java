ef1f146968a16c20baca72467123c6f8f171b31a
王博文 <wangbowen@egoonet.com>
2018-10-22 20:14:31 +0800

    2

--- src/main/java/com/wbw/configuration/SessionConfig.java
+++ src/main/java/com/wbw/configuration/SessionConfig.java
@@ -1,12 +1,12 @@
-package com.wbw.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-/**
- * session共享配置
- */
-@Configuration/**maxInactiveIntervalInSeconds: 设置Session失效时间，使用Redis Session之后，原Boot的server.session.timeout属性不再生效*/
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
-public class SessionConfig {
-}
+//package com.wbw.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//
+///**
+// * session共享配置
+// */
+//@Configuration/**maxInactiveIntervalInSeconds: 设置Session失效时间，使用Redis Session之后，原Boot的server.session.timeout属性不再生效*/
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
+//public class SessionConfig {
+//}
