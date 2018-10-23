c7080a29f4b0ca16930865900a3b2d0ae9e70656
王博文 <wangbowen@egoonet.com>
2018-10-23 21:41:33 +0800

    3

--- src/main/java/com/wbw/controller/UserController.java
+++ src/main/java/com/wbw/controller/UserController.java
@@ -1,37 +1,58 @@
-//package com.wbw.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.domain.Page;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import com.wbw.domain.User;
-//import com.wbw.service.UserRepository;
-//import com.wbw.service.UserService;
-//
-//@Controller
-//@RequestMapping("/user")
-//public class UserController {
-//
-//	@Autowired
-//	private UserRepository userRepository;
-//	@Autowired
-//	private UserService userService;
-//
-//	@RequestMapping("/list")
-//	public String list(Model model) {
-//        model.addAttribute("hello","Hello, Spring Boot!");
-//        model.addAttribute("userList", userRepository.findAll());
-//        return "/user/list";
-//	}
-//	
-//	@ResponseBody
-//	@RequestMapping("/getPers")
-//	public Page<User> getPers(Integer pageNumber, Integer pageSize) {
-//		Page<User> findUserNoCriteria = userService.findUserNoCriteria(pageNumber, pageSize);
-//		return findUserNoCriteria;
-//	}
-//
-//}
+package com.wbw.controller;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wbw.domain.SkuExt;
+import com.wbw.service.UserRepository;
+import com.wbw.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@Resource
+	private UserRepository userRepository;
+	@Resource
+	private UserService userService;
+
+	@RequestMapping("/list")
+	public String list(Model model) {
+        model.addAttribute("hello","Hello, Spring Boot!");
+        model.addAttribute("userList", userRepository.findAll());
+        return "/user/list";
+	}
+	
+	@RequestMapping("/getPers")
+	@ResponseBody
+	public Map<String,Object> listProduct(@RequestParam(value = "limit", required = false) Integer limit, 
+			                              @RequestParam(value = "offset", required = false)Integer offset){
+		Map<String,Object> map = new HashMap<>();
+		List<SkuExt> list = new ArrayList<SkuExt>();//此处应该是从数据库查出来的数据,为了测试方便写个循环
+		for (int i = offset; i < limit+offset; i++) {
+			SkuExt skuExt = new SkuExt();
+			skuExt.setSn(i+"");
+			skuExt.setProductname("商品名称"+i);
+			skuExt.setCost(new BigDecimal(i*100));
+			skuExt.setBrankname("品牌名称"+i);
+			skuExt.setSpecificationvalues("规格值是"+i);
+			skuExt.setAreaname("产地"+i);
+			list.add(skuExt);
+		}
+		map.put("total", 100);//假设共有100条数据
+		map.put("rows", list);
+        return  map;
+	}
+
+}
--- src/main/resources/static/module/list.js
+++ src/main/resources/static/module/list.js
@@ -1,196 +1,80 @@
-var $testTable = $('#testTable');
-$testTable.bootstrapTable({
-    url: '/user/getPers',
-    queryParams: function queryParams(params) {   //设置查询参数，都是可以自动从控件获得
-        var param = {
-            pageNumber: params.pageNumber,
-            pageSize: params.pageSize,
-            search : params.searchText,
-            sortName : params.sortName,
-            sortOrder : params.sortOrder,
-        };
-        return param;
-    },
-    columns: [{
-        field: 'id',
-        title: '编号'
-    }, {
-        field: 'name',
-        title: '姓名'
-    }, {
-        field: 'age',
-        title: '年龄'
-    }, {
-        field: 'address',
-        title: '地址'
-    }, {
-        formatter: function (value, row, index) {
-            return [
-                '<a href="javascript:modifyPer(' + row.id + ",'" + row.name + "'," + row.age + ",'" + row.address + "'" + ')">' +
-                    '<i class="glyphicon glyphicon-pencil"></i>修改' +
-                '</a>',
-                '<a href="javascript:delPer(' + row.id + ')">' +
-                    '<i class="glyphicon glyphicon-remove"></i>删除' +
-                '</a>'
-            ].join('');
-        },
-        title: '操作'
-    }],
-    pageNumber: 1, //初始化加载第一页，默认第一页
-    striped: true,
-    pagination: true,//是否显示分页（*）
-    sidePagination: 'server',//服务端分页
-    pageSize: 10, //每页记录数
-    pageList: [5, 10, 25, 50, 100],//每页的记录行数（*）
-    search: true,  //显示查询框
-    rowStyle: function (row, index) {
-        var ageClass = '';
-        if (row.age < 18) {
-            ageClass = 'text-danger';
-        }
-        return {classes: ageClass}
-    },
+// 对应table标签的id
+$("#indexTable").bootstrapTable({
+	url : "/user/getPers", // AJAX获取表格数据的url
+	striped : true, // 是否显示行间隔色(斑马线)
+	pagination : true, // 是否显示分页（*）
+	//singleSelect : true, // 单选checkbox
+	sidePagination : "server", // 分页方式：client客户端分页，server服务端分页（*）
+	paginationLoop : false, // 当前页是边界时是否可以继续按
+	queryParams : function(params) { // 请求服务器数据时发送的参数，可以在这里添加额外的查询参数，返回false则终止请求
+		return {
+			limit : params.limit, // 每页要显示的数据条数
+			offset : params.offset, // 每页显示数据的开始行号
+		// sort: params.sort, // 要排序的字段
+		// sortOrder: params.order, // 排序规则
+		// dataId: $("#dataId").val() // 额外添加的参数
+		}
+	},// 传递参数（*）
+	pageNumber : 1, // 初始化加载第一页，默认第一页
+	pageSize : 10, // 每页的记录行数（*）
+	pageList : [ 10, 25, 50, 100, 'all' ], // 可供选择的每页的行数（*）
+	contentType : "application/x-www-form-urlencoded",// 一种编码。在post请求的时候需要用到。这里用的get请求，注释掉这句话也能拿到数据
+	// search: true, //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+	strictSearch : false, // 是否全局匹配,false模糊匹配
+	showColumns : true, // 是否显示所有的列
+	showRefresh : true, // 是否显示刷新按钮
+	minimumCountColumns : 2, // 最少允许的列数
+	clickToSelect : false, // 是否启用点击选中行
+	// height: 500, //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+	// uniqueId: "id", //每一行的唯一标识，一般为主键列
+	showToggle : true, // 是否显示详细视图和列表视图的切换按钮
+	cardView : false, // 是否显示详细视图
+	detailView : false, // 是否显示父子表
+	cache : false, // 设置为 false 禁用 AJAX 数据缓存， 默认为true
+	sortable : true, // 是否启用排序
+	sortOrder : "asc", // 排序方式
+	sortName : 'sn', // 要排序的字段
+	columns : [
+//	{
+//		checkbox : true
+//	}, 
+	{
+		field : 'sn', // 返回json数据中的name
+		title : '订货号', // 表格表头显示文字
+		align : 'center', // 左右居中
+		valign : 'middle' // 上下居中
+	}, {
+		field : 'productname',
+		title : '商品名称',
+		align : 'center',
+		valign : 'middle'
+	}, {
+		field : 'cost',
+		title : '价格(¥)',
+		align : 'center',
+		valign : 'middle',
+		sortable : true
+	}, {
+		field : 'brankname',
+		title : '品牌',
+		align : 'center',
+		valign : 'middle',
+	}, {
+		field : 'specificationvalues',
+		title : '规格',
+		align : 'center',
+		valign : 'middle',
+	}, {
+		field : 'areaname',
+		title : '产地',
+		align : 'center',
+		valign : 'middle',
+	} ],
+	onLoadSuccess : function() { // 加载成功时执行
+		console.info("加载成功");
+	},
+	onLoadError : function() { // 加载失败时执行
+		console.info("加载数据失败");
+	}
+
 });
-
-// 设置bootbox中文
-bootbox.setLocale('zh_CN');
-
-var titleTip = '提示';
-
-// 验证输入的年龄是否为数字
-function verifyAge(age) {
-    var reg = /^[0-9]{0,3}$/;
-    if (!reg.test(age)) {
-        return false;
-    }
-    return true;
-}
-
-// 验证姓名和地址是否为空
-function verifyNameAndAddress(name, address) {
-    if (name != '' && address != '') {
-        return true;
-    }
-    return false;
-}
-
-function nullAlert() {
-    bootbox.alert({
-        title: titleTip,
-        message: '所有项均为必填！'
-    });
-}
-
-// 点击查询按钮，年龄符合查询条件方可跳转查询
-$('#queryBtn').click(function () {
-    var age = $('#queryAgeText').val();
-    if (verifyAge(age)) {
-        // 刷新并跳转到第一页
-        $testTable.bootstrapTable('selectPage', 1);
-    } else {
-        bootbox.alert({
-           title: titleTip,
-           message: '年龄输入有误！'
-        });
-    }
-});
-
-// 点击重置按钮，清空查询条件并跳转回第一页
-$('#resetBtn').click(function() {
-    $('.form-group :text').val('');
-    $testTable.bootstrapTable('selectPage', 1);
-});
-
-// 用于修改服务器资源
-function exchangeData(path, id, name, age, address) {
-    $.ajax({
-        url: path,
-        type: 'post',
-        data : {
-            id: id,
-            name: name,
-            age: age,
-            address: address
-        },
-        success: function(res) {
-            bootbox.alert({
-                title: titleTip,
-                message: res.message
-            });
-            // 在每次提交操作后返回首页
-            $testTable.bootstrapTable('selectPage', 1);
-        }
-    });
-}
-
-// 新增
-$('#addBtn').click(function() {
-    $('#addNameText').val('');
-    $('#addAgeText').val('');
-    $('#addAddressText').val('');
-    $('#addModal').modal('show');
-});
-
-$('#saveAdd').click(function() {
-    $('#addModal').modal('hide');
-    bootbox.confirm({
-        title: titleTip,
-        message: '确认增加？',
-        callback: function (flag) {
-            if (flag) {
-                var name = $('#addNameText').val();
-                var age = $('#addAgeText').val();
-                var address = $('#addAddressText').val();
-                if (verifyNameAndAddress(name, address)) {
-                    exchangeData('addPer', null, name, age, address);
-                } else {
-                    nullAlert();
-                }
-            }
-        }
-    });
-});
-
-var mid;
-
-// 修改
-function modifyPer(id, name, age, address) {
-    mid = id;
-    $('#modifyNameText').val(name);
-    $('#modifyAgeText').val(age);
-    $('#modifyAddressText').val(address);
-    $('#modifyModal').modal('show');
-}
-
-$('#saveModify').click(function() {
-    $('#modifyModal').modal('hide');
-    bootbox.confirm({
-        title: titleTip,
-        message: '确认修改？',
-        callback: function (flag) {
-            if (flag) {
-                var name = $('#modifyNameText').val();
-                var age = $('#modifyAgeText').val();
-                var address = $('#modifyAddressText').val();
-                if (verifyNameAndAddress(name, address)) {
-                    exchangeData('modifyPer', mid, name, age, address);
-                } else {
-                    nullAlert();
-                }
-            }
-        }
-    });
-});
-
-// 删除
-function delPer(id) {
-    bootbox.confirm({
-        title: titleTip,
-        message: '确认删除？',
-        callback: function(flag) {
-            if (flag) {
-                exchangeData("delPer", id);
-            }
-        }
-    });
-}
